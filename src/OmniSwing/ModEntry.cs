6432a4838e3c4280750887bc16c0a97fd79f6cbf
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-30 20:52:26 +0100

    feat: create ModEntry

---
+++ src/OmniSwing/ModEntry.cs
@@ -0,0 +1,14 @@
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace OmniSwing
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
